9849dd8c48005269827ae992d28b66596667cc87
Roman Rodionov <rodionov.rn@yandex.ru>
2021-03-21 03:39:34 +0300

    Add tests for main.download

--- ftp_loader/main.py
+++ ftp_loader/main.py
@@ -12,33 +12,26 @@ def read_config(config_file):
     file_trans = loader.create_file_transfers(path, files)
     return url, file_trans
 
-def auth(url):
+def auth():
     print('Please, enter your login and password to access FTP server. \n')
     user = input('Username: ')
     passwd = getpass.getpass('Password: ')
     return user, passwd
 
 
-def download_data(url, file_trans, skip_existing):
-    print('Start downloading project data from {0}'.format(url))
-    user, passwd = auth(url)
-
+def download_data(url, user, passwd, file_trans, skip_existing, **kwargs):
     downloaded = []
-    with Connection(url, user, password=passwd) as conn:
+    with Connection(url, user, password=passwd, **kwargs) as conn:
         for ft in file_trans:
             try:
                 ft.download(conn, skip_existing)
                 downloaded.append(ft)
             except loader.LoaderException as e:
                 print(e.message)
-        print('Finished. {0} files were loaded.\n'.format(len(downloaded)))
     return downloaded
 
 
-def upload_data(url, file_trans, skip_existing):
-    print('Start uploading project data to {0}'.format(url))
-    user, passwd = auth(url)
-
+def upload_data(url, user, passwd, file_trans, skip_existing):
     with Connection(url, user, password=passwd) as conn:
         count = 0
         for ft in file_trans:
@@ -47,7 +40,7 @@ def upload_data(url, file_trans, skip_existing):
                 count += 1
             except loader.LoaderException as e:
                 print(e.message)
-        print('Finished. {0} files were uploaded.\n'.format(count))
+    return count
 
 
 def decompress_data(file_trans, skip_existing):
@@ -108,9 +101,15 @@ def main():
     if args['clear']:
         clear_data(file_trans)
     elif args['upload']:
+        print('Start uploading project data to {0}'.format(url))
+        user, passwd = auth()
         compress_data(file_trans, skip_existing)
-        upload_data(url, file_trans, skip_existing)
+        count = upload_data(url, user, passwd, file_trans, skip_existing)
+        print('Finished. {0} files were uploaded.\n'.format(count))
     else:
-        downloaded = download_data(url, file_trans, skip_existing)
-        decompress_data(downloaded, skip_existing)
+        print('Start downloading project data from {0}'.format(url))
+        user, passwd = auth()
+        downloaded = download_data(url, user, passwd, file_trans, skip_existing)
+        print('Finished. {0} files were loaded.\n'.format(len(downloaded)))
+        decompress_data(file_trans, skip_existing)
     print('Done. \n')
---
+++ tests/test_main.py
@@ -0,0 +1,105 @@
+import pytest
+from pathlib import Path, PurePosixPath
+import bz2, gzip
+from pysftp import Connection, CnOpts
+
+from ftp_loader import loader
+from ftp_loader.main import read_config, download_data
+from tests.test_loader import create_temp_file
+
+
+def clear_dir(path):
+    for p in path.iterdir():
+        if p.is_dir():
+            clear_dir(p)
+            print('Delete dir ' + str(p))
+            p.rmdir()
+        else:
+            print('Delete file ' + str(p))
+            p.unlink()
+            
+
+
+
+@pytest.fixture(scope='function')
+def ftp_server1(sftpserver):
+    data = {
+        'project1': {
+            'test_data1': {
+                'file1.txt.bz2': bz2.compress(b'File1 content'),
+                'file2.txt.bz2': bz2.compress(b'File2 content'),
+            },
+            'test_data2': {
+                'container': {
+                    'file21.csv.gz': gzip.compress(b'File21 content'),
+                    'file22.csv.gz': gzip.compress(b'FIle22 content')
+                },
+                'container2.txt': 'Container2 content'
+            }
+        },
+        'project2': {
+            'readme.txt': 'Readme file',
+            'data3': {
+                'file31.txt': 'File31 content',
+                'file32.txt': 'File32 content'
+            }
+        }
+    }
+    with sftpserver.serve_content(data):
+        yield sftpserver
+
+
+@pytest.fixture(scope='function')
+def config1(tmp_path):
+    f = tmp_path / 'ftp-config.toml'
+    f.write_text('\n'.join([
+        'url = "localhost"',
+        'path = "project1"',
+        '[[files]]',
+        'dst = ' + '"{0}"'.format(str(tmp_path / "work1")).replace('\\', '\\\\'),
+        'src = "test_data1"', 
+        'arch = "bz2"', 
+        'names = ["file1.txt", "file2.txt"]',
+        '[[files]]',
+        'dst = ' + '"{0}"'.format(str(tmp_path / "work2/cont2")).replace('\\', '\\\\'), 
+        'src = "test_data2/container"', 
+        'arch = "gz"',
+        'names = ["file21.csv", "file22.csv"]',
+        '[[files]]', 
+        'dst = ' + '"{0}"'.format(str(tmp_path / "work2")).replace('\\', '\\\\'), 
+        'src = "test_data2"', 
+        'names = ["container2.txt"]'
+    ]))
+    yield
+    clear_dir(tmp_path)
+
+
+@pytest.mark.parametrize('download_cnt', [5])
+def test_download(ftp_server1, config1, tmp_path, download_cnt):
+    url, file_trans = read_config(tmp_path / 'ftp-config.toml')
+    port = ftp_server1.port
+    cnopts = CnOpts()
+    cnopts.hostkeys = None
+    downloads = download_data(url, 'user1', '1234', file_trans, False, port=port, cnopts=cnopts)
+    assert len(downloads) == download_cnt
+
+
+@pytest.fixture(scope='function')
+def file_tree1(tmp_path):
+    create_temp_file(tmp_path / "work1", 'file1.txt', 'Not File1 content', False)
+    create_temp_file(tmp_path / "work2/cont2", 'file21.csv', 'Not File21 content', False)
+    #create_temp_file(tmp_path / "work2", 'container2.txt', 'Not Container2 content', False)
+    create_temp_file(tmp_path / "work2/cont2", 'file22.csv.gz', gzip.compress(b'Not file 22'), True)
+    yield
+    clear_dir(tmp_path)
+
+
+@pytest.mark.parametrize('download_cnt, skip', [(5, False), (2, True)])
+def test_download2(ftp_server1, config1, file_tree1, tmp_path, download_cnt, skip):
+    url, file_trans = read_config(tmp_path / 'ftp-config.toml')
+    port = ftp_server1.port
+    cnopts = CnOpts()
+    cnopts.hostkeys = None
+    downloads = download_data(url, 'user1', '1234', file_trans, skip, port=port, cnopts=cnopts)
+    assert len(downloads) == download_cnt
+
